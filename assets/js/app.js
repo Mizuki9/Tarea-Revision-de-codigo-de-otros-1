d2948c24ed3aa2bde1d5a10e88f79d7ccd64aad0
Mirta Tapia <mirta.atd@gmail.com>
2023-11-27 15:07:43 -0600

    worked on corrections

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,43 @@
-const baseEndpoint = 'https://api.github.com';
-const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
 
-function displayUser(username) {
-  $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+/* Al tener "await" esta función debe ser async, lo agregué */
+async function displayUser(url, username) {
+
+
+  /* Name es una clase, agregué su . para que quedara en .name */
+  const name = document.querySelector('.name');
+
+  /* Blog es una clase, no un ID, cambié a .blog */
+  const blog = document.querySelector('.blog');
+
+  /*
+  No encontré "location" dentro del HTMLAllCollection, lo cual me llevó a la conclusión de que no existe: "If no matches are found, null is returned."
+  
+  Lo agregaré en el HTML.
+  */
+  const location = document.querySelector('.location');
+
+  /* Los nombres de las variables de todas las líneas arriba de este comentario no me parecen muy claros, así que los cambié. También las metí todas dentro de la función ya que no es necesario que sea variables globales. La url la convertí en un parámetro, y la otra variable la eliminé para reducir código */
+
+
+  name.textContent = 'cargando...';
+
+  /* Al ser una promesa, le agregué try y catch */
+  try {
+    const response = await fetch(`${url}/${username}`);
+    console.log(data);
+    name.textContent = '${data.name}';
+    blog.textContent = '${data.blog}';
+    location.textContent = '${data.location}';
+  } catch (error) {
+    /* Fusioné esta sección con la función de arriba y la coloqué en la sección de catch */
+    console.log('OH NO!');
+    console.log(error);
+    n.textContent = `Algo salió mal: ${error}`
+  }
+
 }
 
-function handleError(err) {
-  console.log('OH NO!');
-  console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
-}
+displayUser('https://api.github.com', 'stolinski');
+/* Removí el .catch porque puse la función en la sección de catch en su lugar */
 
-displayUser('stolinski').catch(handleError);
+/* Me he hecho al hábito de usar Shift + Alt + F así que el formato del documento también lo podría haber cambiado */
